60ceaae1b01fdce78e6370c45aa14ef027da641b
Tilerphy <346410840@163.com>
2017-04-17 16:23:24 +0800

    sql helper

---
+++ server/config.js
@@ -0,0 +1,10 @@
+var config = {
+	"mysql":{
+        	"host":"localhost",
+        	"user":"root",
+       		"password":"1qaz2wsxE",
+        	"database":"coveryoureyes",
+    	}
+};
+
+module.exports=config;
