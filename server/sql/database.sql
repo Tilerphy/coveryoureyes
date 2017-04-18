e7c220ed355b10e0e9e28f8b90e8e6abe367764b
Zonas Sun <346410840@163.com>
2017-04-18 14:32:41 +0800

    Update database.sql

--- server/sql/database.sql
+++ server/sql/database.sql
@@ -5,12 +5,12 @@ create database coveryoureyes;
 use coveryoureyes;
 create table trophy(id varchar(64) primary key, gameid varchar(64), name varchar(128), trophypoint int);
 create table game(id varchar(64) primary key, name varchar(128));
-create table alluser(fedid varchar(128) primary key, nickname varchar(128), others text);
+create table alluser(id varchar(64) primary key ,fedid varchar(128), nickname varchar(128), others text);
 create table trophyrecord(id varchar(64) primary key,fedid varchar(128), recordcount int, needcount int, trophy varchar(64), createtime bigint, endtime 
 bigint);
 create table ranking(id varchar(64) primary key, fedid varchar(128), gameid varchar(64), highscore bigint, rank int, createtime bigint);
 create index trophy_index on trophy(id, gameid);
-create index alluser_index on alluser(fedid, nickname);
+create index alluser_index on alluser(fedid, nickname, id);
 create index game_index on  game(id, name);
 create index tr_index on trophyrecord(id, fedid, trophy);
 create index ranking_index on ranking(fedid, id, gameid, rank);
