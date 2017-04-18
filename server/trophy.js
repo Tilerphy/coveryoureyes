c288c6b0f911d54024d58a38b6ff0372685fd69c
Tilerphy <346410840@163.com>
2017-04-18 17:22:24 +0800

    update trophy info

--- server/trophy.js
+++ server/trophy.js
@@ -18,4 +18,40 @@ trophy.get("/info", (req,res)=>{
 		res.end();
 	}
 });
+
+trophy.get("/init", (req,res)=>{
+
+
+});
+
+trophy.get("/update", (req,res)=>{
+	var fedId = req.query("fedId");
+	var recordId =req.query("recordId");
+	var val = req.query("val");
+	if(fedId && recordId && val){
+		helper.exists("trophyrecord", "recordId=? and fedid=?", [recordId, fedId], 
+		()=>{
+			helper.update("trophyrecord",{"endtime":Date.now(), "recordcount":val}  "recordId=? and fedId=?", [recordId, fedId], (err, 
+result)=>{
+				if(err){
+                                	res.status(400);
+                                	res.json(err);
+                                	res.end();
+                        	}else{
+                                	res.json(result);
+                                	res.end();
+                        	}
+			});
+		}, 
+		()=>{
+			res.status(400);
+			res.end();
+		});
+	}else{
+		res.status(400);
+                res.end();
+	}
+});
+
+
 module.exports = trophy;
