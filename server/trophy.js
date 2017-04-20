a4d7c468c6abe87317e97bf956f5b6a8d1e799fa
Tilerphy <346410840@163.com>
2017-04-20 16:24:40 +0800

    change database

--- server/trophy.js
+++ server/trophy.js
@@ -3,7 +3,7 @@ var helper = require("./sql");
 trophy.get("/info", (req,res)=>{
 	var trophyId = req.query["id"];
 	if(trophyId){
-		helper.query("trophy", "id=?", [trophyId], ["id","gameid","name","trophypoint","icon"], (err, result)=>{
+		helper.query("trophy", "id=?", [trophyId], ["id","gameid","name","trophypoint","icon","description"], (err, result)=>{
 			if(err){
 				res.status(400);
 				res.json(err);
