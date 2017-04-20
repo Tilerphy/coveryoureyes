25147e967326148817ee2a5d2a7016fceb9934d5
Zonas Sun <346410840@163.com>
2017-04-20 16:14:01 +0800

    Update coveryoureyes.sql

--- server/sql/games/coveryoureyes.sql
+++ server/sql/games/coveryoureyes.sql
@@ -1,5 +1,7 @@
 insert into game (id, name, icon, description) values ("g000001","Cover your eyes", "","");
-insert into trophy(id,gameid,name,trophypoint,icon) values("t000001", "g000001","感谢玩耍！",5, "");
-insert into trophy(id,gameid,name,trophypoint,icon) values("t000002", "g000001","只需一秒！", 5, "");
-insert into trophy(id,gameid,name,trophypoint,icon) values("t000003", "g000001","你在做什么？", 5, "");
-
+insert into trophy(id,gameid,name,trophypoint,icon,description) values("t000001", "g000001","感谢玩耍！",5, "","进行了一次游戏。");
+insert into trophy(id,gameid,name,trophypoint,icon,description) values("t000002", "g000001","只需一秒！", 5, "有效计分时间超过1秒。");
+insert into trophy(id,gameid,name,trophypoint,icon,description) values("t000003", "g000001","你在做什么？", 5, "在一局游戏中获得0分。");
+insert into trophy(id,gameid,name,trophypoint,icon,description) values("t000004", "g000001","十分美", 10, "累计有效计分时间超过10分钟。");
+insert into trophy(id,gameid,name,trophypoint,icon,description) values("t000005", "g000001","余音绕梁", 10, "让人痴迷的声音，你玩了100次。");
+insert into trophy(id,gameid,name,trophypoint,icon,description) values("t000006", "g000001","嘈杂人生", 50, "累计分数超过2147483647分。");
