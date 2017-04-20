34c551a2f300da226c7c70bae6262c60ba980f16
Zonas Sun <346410840@163.com>
2017-04-20 16:09:35 +0800

    Update coveryoureyes.sql

--- server/sql/games/coveryoureyes.sql
+++ server/sql/games/coveryoureyes.sql
@@ -1,5 +1,5 @@
 insert into game (id, name, icon, description) values ("g000001","Cover your eyes", "","");
-insert into trophy(id,gameid,name,trophypoint,icon) values("t000001", "g000001","Thanks for playing!",5, "");
-insert into trophy(id,gameid,name,trophypoint,icon) values("t000002", "g000001","1000000 points!", 5, "");
-insert into trophy(id,gameid,name,trophypoint,icon) values("t000003", "g000001","0 point!", 5, "");
+insert into trophy(id,gameid,name,trophypoint,icon) values("t000001", "g000001","感谢玩耍！",5, "");
+insert into trophy(id,gameid,name,trophypoint,icon) values("t000002", "g000001","只需一秒！", 5, "");
+insert into trophy(id,gameid,name,trophypoint,icon) values("t000003", "g000001","你在做什么？", 5, "");
 
