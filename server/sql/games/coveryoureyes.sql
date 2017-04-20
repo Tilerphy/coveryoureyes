5b424410274d06c17c0294c1dece9d6a978338b2
Tilerphy <346410840@163.com>
2017-04-20 16:04:59 +0800

    add datas

---
+++ server/sql/games/coveryoureyes.sql
@@ -0,0 +1,5 @@
+insert into game (id, name, icon, description) values ("g000001","Cover your eyes", "","");
+insert into trophy(id,gameid,name,trophypoint,icon) values("t000001", "g000001","Thanks for playing!",5, "");
+insert into trophy(id,gameid,name,trophypoint,icon) values("t000002", "g000001","1000000 points!", 5, "");
+insert into trophy(id,gameid,name,trophypoint,icon) values("t000003", "g000001","0 point!", 5, "");
+
