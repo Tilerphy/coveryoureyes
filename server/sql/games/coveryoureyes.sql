06d2b75224803c0ebea73884dedc2134173ac58e
Zonas Sun <346410840@163.com>
2017-04-20 16:40:39 +0800

    Update coveryoureyes.sql

--- server/sql/games/coveryoureyes.sql
+++ server/sql/games/coveryoureyes.sql
@@ -3,5 +3,7 @@ insert into trophy(id,gameid,name,trophypoint,icon,description) values("t000001"
 insert into trophy(id,gameid,name,trophypoint,icon,description) values("t000002", "g000001","只需一秒！", 5, "","有效计分时间超过1秒。");
 insert into trophy(id,gameid,name,trophypoint,icon,description) values("t000003", "g000001","你在做什么？", 5,"", "在一局游戏中获得0分。");
 insert into trophy(id,gameid,name,trophypoint,icon,description) values("t000004", "g000001","十分美", 10,"", "累计有效计分时间超过10分钟。");
+insert into trophy(id,gameid,name,trophypoint,icon,description) values("t000007", "g000001","完美！", 50,"", "一局中95%以上时间都是有效计分时间。");
 insert into trophy(id,gameid,name,trophypoint,icon,description) values("t000005", "g000001","余音绕梁", 10,"", "让人痴迷的声音，你玩了100次。");
 insert into trophy(id,gameid,name,trophypoint,icon,description) values("t000006", "g000001","嘈杂人生", 50,"", "累计分数超过2147483647分。");
+insert into trophy(id,gameid,name,trophypoint,icon,description) values("t000008", "g000001","完美？", 10,"", "一局中90%以上时间都是有效计分时间。");
